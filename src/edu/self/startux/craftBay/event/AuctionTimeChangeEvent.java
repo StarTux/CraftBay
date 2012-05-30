94bea422d192304a78e74c5e0392e45e94fa3743
Thomas Loy <ths.loy@googlemail.com>
2012-05-30 23:45:52 +0200

    initial commit

---
+++ src/edu/self/startux/craftBay/event/AuctionTimeChangeEvent.java
@@ -0,0 +1,54 @@
+/* * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *
+ * Copyright 2012 StarTux
+ *
+ * This file is part of CraftBay.
+ *
+ * CraftBay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * CraftBay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with CraftBay.  If not, see <http://www.gnu.org/licenses/>.
+ * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * */
+
+package edu.self.startux.craftBay.event;
+
+import edu.self.startux.craftBay.Auction;
+import org.bukkit.command.CommandSender;
+import org.bukkit.event.Event;
+import org.bukkit.event.HandlerList;
+
+public class AuctionTimeChangeEvent extends AuctionEvent {
+        private static HandlerList handlers = new HandlerList();
+        private CommandSender sender;
+        private int delay;
+
+        public AuctionTimeChangeEvent(Auction auction, CommandSender sender, int delay) {
+                super(auction);
+                this.sender = sender;
+                this.delay = delay;
+        }
+
+        public static HandlerList getHandlerList() {
+                return handlers;
+        }
+
+        @Override
+        public HandlerList getHandlers() {
+                return handlers;
+        }
+
+        public CommandSender getSender() {
+                return sender;
+        }
+
+        public int getDelay() {
+                return delay;
+        }
+}
