0f37d02bcab5209d0400121cf3a8080575fd1520
StarTux <ths.loy@googlemail.com>
2018-08-02 20:30:43 +0200

    Simplify language loading and parsing; add Russian translation

--- src/main/java/edu/self/startux/craftBay/locale/Language.java
+++ src/main/java/edu/self/startux/craftBay/locale/Language.java
@@ -20,135 +20,96 @@
 package edu.self.startux.craftBay.locale;
 
 import edu.self.startux.craftBay.CraftBayPlugin;
-import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.configuration.file.YamlConfiguration;
 
-public class Language {
-        private CraftBayPlugin plugin;
-        private Map<String, Message> map = new HashMap<String, Message>();
+public final class Language {
+    private CraftBayPlugin plugin;
+    private Map<String, Message> map = new HashMap<String, Message>();
 
-        public Language(CraftBayPlugin plugin, String locale) {
-                this.plugin = plugin;
-                loadLanguageResource("en_US");
-                loadLanguageFile(locale);
-        }
+    public Language(CraftBayPlugin plugin, String locale) {
+        this.plugin = plugin;
+        loadLanguageResource("en_US");
+        loadLanguageFile(locale);
+    }
 
-        public Language(CraftBayPlugin plugin) {
-                this(plugin, "en_US");
-        }
+    public Language(CraftBayPlugin plugin) {
+        this(plugin, "en_US");
+    }
 
-        public static void writeLanguageFiles() {
-                CraftBayPlugin plugin = CraftBayPlugin.getInstance();
-                BufferedReader reader = new BufferedReader(new InputStreamReader(plugin.getResource("lang/list")));
-                while (true) {
-                        String line = null;
-                        try {
-                                line = reader.readLine();
-                        } catch (IOException ioe) {
-                                ioe.printStackTrace();
-                                return;
-                        }
-                        if (line == null) break;
-                        InputStream inp = plugin.getResource("lang/" + line);
-                        if (inp == null) {
-                                plugin.getLogger().severe("Unable to locate resource: \"lang/" + line + "\"");
-                                continue;
-                        }
-                        File file = plugin.getDataFolder();
-                        if (!file.exists()) file.mkdir();
-                        file = new File(file, "lang");
-                        if (!file.exists()) file.mkdir();
-                        file = new File(file, line);
-                        // if (file.exists()) continue;
-                        try {
-                                FileOutputStream out = new FileOutputStream(file);
-                                while (true) {
-                                        int b = inp.read();
-                                        if (b == -1) break;
-                                        out.write(b);
-                                }
-                                out.close();
-                        } catch (IOException ioe) {
-                                plugin.getLogger().warning("Error while trying to write \"" + file.getPath() + "\"");
-                                continue;
-                        }
-                        try {
-                                inp.close();
-                        } catch (IOException ioe) {
-                                ioe.printStackTrace();
-                        }
-                }
+    public static void writeLanguageFiles() {
+        CraftBayPlugin plugin = CraftBayPlugin.getInstance();
+        for (String str: Arrays.asList("de_DE", "en_US", "zh_CN", "ru_RU")) {
+            plugin.saveResource("lang/" + str + ".yml", false);
         }
+    }
 
-        private void loadLanguageResource(String name) {
-                String path = "lang/" + name + ".yml";
-                InputStream inp = plugin.getResource(path);
-                if (inp == null) {
-                        plugin.getLogger().warning("Language file not found: \"" + name + "\"");
-                        return;
-                }
-                loadLanguageStream(inp);
+    private void loadLanguageResource(String name) {
+        String path = "lang/" + name + ".yml";
+        InputStream inp = plugin.getResource(path);
+        if (inp == null) {
+            plugin.getLogger().warning("Language file not found: \"" + name + "\"");
+            return;
         }
+        loadLanguageStream(inp);
+    }
 
-        private void loadLanguageFile(String name) {
-                File file = plugin.getDataFolder();
-                file = new File(file, "lang");
-                file = new File(file, name + ".yml");
-                try {
-                        loadLanguageStream(new FileInputStream(file));
-                } catch (FileNotFoundException fnfe) {
-                        plugin.getLogger().warning("Unable to locate locale file: \"" + file.getPath() + "\"");
-                        return;
-                }
+    private void loadLanguageFile(String name) {
+        File file = plugin.getDataFolder();
+        file = new File(file, "lang");
+        file = new File(file, name + ".yml");
+        try {
+            loadLanguageStream(new FileInputStream(file));
+        } catch (FileNotFoundException fnfe) {
+            plugin.getLogger().warning("Unable to locate locale file: \"" + file.getPath() + "\"");
+            return;
         }
+    }
 
-        @SuppressWarnings("unchecked")
-        private void loadLanguageStream(InputStream inp) {
-                ConfigurationSection conf = YamlConfiguration.loadConfiguration(new InputStreamReader(inp));
-                for (String key : conf.getKeys(true)) {
-                        Message message;
-                        Object o = conf.get(key);
-                        if (o instanceof List) {
-                                message = new Message((List<String>)o);
-                        } else if (o instanceof String) {
-                                message = new Message((String)o);
-                        } else {
-                                continue;
-                        }
-                        map.put(key.toLowerCase(), message);
-                        // System.out.println(key);
-                        // System.out.println(message.toString());
-                }
+    @SuppressWarnings("unchecked")
+    private void loadLanguageStream(InputStream inp) {
+        ConfigurationSection conf = YamlConfiguration.loadConfiguration(new InputStreamReader(inp));
+        for (String key : conf.getKeys(true)) {
+            Message message;
+            Object o = conf.get(key);
+            if (o instanceof List) {
+                message = new Message((List<String>)o);
+            } else if (o instanceof String) {
+                message = new Message((String)o);
+            } else {
+                continue;
+            }
+            map.put(key.toLowerCase(), message);
+            // System.out.println(key);
+            // System.out.println(message.toString());
         }
+    }
 
-        public Message getMessage(String key) {
-                if (key == null) return new Message();
-                Message result = map.get(key.toLowerCase());
-                if (result == null) {
-                        System.err.println("Language key not found: " + key);
-                        return new Message();
-                }
-                return result.clone();
+    public Message getMessage(String key) {
+        if (key == null) return new Message();
+        Message result = map.get(key.toLowerCase());
+        if (result == null) {
+            System.err.println("Language key not found: " + key);
+            return new Message();
         }
+        return result.clone();
+    }
 
-        public Message getMessages(String... keys) {
-                Message result = new Message();
-                for (String key : keys) {
-                        if (key == null) continue;
-                        result.append(getMessage(key));
-                }
-                return result;
+    public Message getMessages(String... keys) {
+        Message result = new Message();
+        for (String key : keys) {
+            if (key == null) continue;
+            result.append(getMessage(key));
         }
+        return result;
+    }
 }
--- src/main/java/edu/self/startux/craftBay/locale/Message.java
+++ src/main/java/edu/self/startux/craftBay/locale/Message.java
@@ -21,10 +21,8 @@ package edu.self.startux.craftBay.locale;
 
 import edu.self.startux.craftBay.Auction;
 import edu.self.startux.craftBay.AuctionTime;
-import edu.self.startux.craftBay.CraftBayPlugin;
 import edu.self.startux.craftBay.Item;
 import edu.self.startux.craftBay.Merchant;
-import edu.self.startux.craftBay.MoneyAmount;
 import edu.self.startux.craftBay.event.AuctionBidEvent;
 import edu.self.startux.craftBay.event.AuctionTimeChangeEvent;
 import java.util.ArrayList;
@@ -35,232 +33,236 @@ import java.util.ListIterator;
 import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import org.bukkit.ChatColor;
 import org.bukkit.command.CommandSender;
 
-public class Message {
-        private static Object endl = new Object();
-        private static class Variable {
-                public String key;
-        }
-        private static class Escape {
-                public String sequence;
-                @Override public String toString() { return sequence; }
-        }
-
-        private LinkedList<Object> tokens = new LinkedList<Object>();
-        private Map<String, Object> environment = new HashMap<String, Object>();
-
-        public Message(List<String> input) {
-                for (String inp : input) {
-                        tokens.add(inp);
-                        tokens.add(endl);
-                }
-                filterEscapes();
-                filterColors();
-                filterVariables();
-        }
-
-        public Message(String input) {
-                tokens.add(input);
-                tokens.add(endl);
-                filterEscapes();
-                filterColors();
-                filterVariables();
-        }
-
-        /**
-         * Empty message
-         */
-        public Message() {
-        }
-
-        public Message append(Message other) {
-                tokens.addAll(other.tokens);
-                environment.putAll(other.environment);
-                return this;
-        }
-
-        /**
-         * Copy constructor
-         */
-        public Message(Message other) {
-                tokens = other.tokens;
-                environment = new HashMap<String, Object>(other.environment);
-        }
-
-        private void filterEscapes() {
-                ListIterator<Object> iter = tokens.listIterator();
-                while (iter.hasNext()) {
-                        Object o = iter.next();
-                        if (!(o instanceof String)) continue;
-                        iter.remove();
-                        String string = (String)o;
-                        Pattern pattern = Pattern.compile("\\\\(.)");
-                        Matcher matcher = pattern.matcher(string);
-                        int lastIndex = 0;
-                        while (matcher.find()) {
-                                iter.add(string.substring(lastIndex, matcher.start()));
-                                iter.add(matcher.group(1));
-                                lastIndex = matcher.end();
-                        }
-                        iter.add(string.substring(lastIndex, string.length()));
-                }
-        }
-
-        private void filterColors() {
-                ListIterator<Object> iter = tokens.listIterator();
-                while (iter.hasNext()) {
-                        Object o = iter.next();
-                        if (!(o instanceof String)) continue;
-                        iter.remove();
-                        String string = (String)o;
-                        Pattern pattern = Pattern.compile("<([^>]+)>");
-                        Matcher matcher = pattern.matcher(string);
-                        int lastIndex = 0;
-                        while (matcher.find()) {
-                                Color color = Color.getByName(matcher.group(1));
-                                if (color == null) continue;
-                                iter.add(string.substring(lastIndex, matcher.start()));
-                                lastIndex = matcher.end();
-                                iter.add(color);
-                        }
-                        iter.add(string.substring(lastIndex, string.length()));
-                }
-        }
-
-        private void filterVariables() {
-                ListIterator<Object> iter = tokens.listIterator();
-                while (iter.hasNext()) {
-                        Object o = iter.next();
-                        if (!(o instanceof String)) continue;
-                        iter.remove();
-                        String string = (String)o;
-                        Pattern pattern = Pattern.compile("\\{([^\\}]+)\\}");
-                        Matcher matcher = pattern.matcher(string);
-                        int lastIndex = 0;
-                        while (matcher.find()) {
-                                iter.add(string.substring(lastIndex, matcher.start()));
-                                lastIndex = matcher.end();
-                                Variable var = new Variable();
-                                var.key = matcher.group(1);
-                                environment.put(var.key, "{" + matcher.group(1) + "}");
-                                iter.add(var);
-                        }
-                        iter.add(string.substring(lastIndex, string.length()));
-                }
-        }
-
-        public List<String> compile() {
-                List<String> result = new ArrayList<String>();
-                StringBuilder sb = new StringBuilder();
-                for (Object o : tokens) {
-                        if (o == endl) {
-                                result.add(sb.toString());
-                                sb = new StringBuilder();
-                        } else if (o instanceof Variable) {
-                                sb.append(environment.get(((Variable)o).key).toString());
-                        } else {
-                                sb.append(o.toString());
-                        }
-                }
-                return result;
-        }
-
-        public List<String> compileNoColor() {
-                List<String> result = new ArrayList<String>();
-                StringBuilder sb = new StringBuilder();
-                for (Object o : tokens) {
-                        if (o == endl) {
-                                result.add(sb.toString());
-                                sb = new StringBuilder();
-                        } else if (o instanceof Variable) {
-                                sb.append(environment.get(((Variable)o).key).toString());
-                        } else if (o instanceof Color) {
-                                // do nothing
-                        } else {
-                                sb.append(o.toString());
-                        }
-                }
-                return result;
-        }
-
-        public Message set(String key, Object value) {
-                environment.put(key, value);
-                return this;
-        }
-
-        public Message set(Item item) {
-                set("item", item.getName());
-                set("itemdesc", item.getDescription());
-                set("amount", item.getAmount());
-                set("totalamount", item.getAmount().getInt());
-                set("enchantments", item.getItemInfo());
-                set("iteminfo", item.getItemInfo());
-                return this;
-        }
-
-        public Message set(Auction auction) {
-                set("id", auction.getId());
-                set(auction.getItem());
-                set("owner", auction.getOwner().getName());
-                if (auction.getWinner() != null) set("winner", auction.getWinner().getName());
-                set("minbid", auction.getMinimalBid());
-                set("maxbid", auction.getMaxBid());
-                set("price", auction.getWinningBid());
-                set("fee", auction.getFee());
-                set("state", auction.getState());
-                set("timeleft", new AuctionTime(auction.getTimeLeft()));
-                return this;
-        }
-
-        public Message set(CommandSender sender) {
-                set("player", sender.getName());
-                return this;
-        }
-
-        public Message set(Merchant merchant) {
-                set("player", merchant.getName());
-                return this;
-        }
-
-        public Message set(Auction auction, CommandSender sender) {
-                set(auction);
-                set(sender);
-                return this;
-        }
-
-        public Message set(Auction auction, Merchant merchant) {
-                set(auction);
-                set(merchant);
-                return this;
-        }
-
-        public Message set(AuctionBidEvent event) {
-                set(event.getAuction());
-                set(event.getBidder());
-                set("bid", event.getAmount());
-                if (event.getOldWinner() != null) set("oldwinner", event.getOldWinner().getName());
-                set("oldprice", event.getOldPrice());
-                return this;
-        }
-
-        public Message set(AuctionTimeChangeEvent event) {
-                set(event.getAuction());
-                set(event.getSender());
-                set("newtimeleft", new AuctionTime(event.getDelay()));
-                return this;
-        }
-
-        @Override
-        protected Message clone() {
-                return new Message(this);
-        }
-
+public final class Message {
+    private static Object endl = new Object();
+    private static class Variable {
+        private String key;
+    }
+    private static class Escape {
+        private String sequence;
         @Override
         public String toString() {
-                StringBuilder sb = new StringBuilder();
-                for (String line : compileNoColor()) sb.append(line).append('\n');
-                if (sb.length() > 0) sb.setLength(sb.length() - 1);
-                return sb.toString();
+            return sequence;
         }
+    }
+
+    private LinkedList<Object> tokens = new LinkedList<Object>();
+    private Map<String, Object> environment = new HashMap<String, Object>();
+
+    public Message(List<String> input) {
+        for (String inp : input) {
+            tokens.add(inp);
+            tokens.add(endl);
+        }
+        filterEscapes();
+        filterColors();
+        filterVariables();
+    }
+
+    public Message(String input) {
+        tokens.add(input);
+        tokens.add(endl);
+        filterEscapes();
+        filterColors();
+        filterVariables();
+    }
+
+    /**
+     * Empty message
+     */
+    public Message() {
+    }
+
+    public Message append(Message other) {
+        tokens.addAll(other.tokens);
+        environment.putAll(other.environment);
+        return this;
+    }
+
+    /**
+     * Copy constructor
+     */
+    public Message(Message other) {
+        tokens = other.tokens;
+        environment = new HashMap<String, Object>(other.environment);
+    }
+
+    private void filterEscapes() {
+        ListIterator<Object> iter = tokens.listIterator();
+        while (iter.hasNext()) {
+            Object o = iter.next();
+            if (!(o instanceof String)) continue;
+            iter.remove();
+            String string = (String)o;
+            Pattern pattern = Pattern.compile("\\\\(.)");
+            Matcher matcher = pattern.matcher(string);
+            int lastIndex = 0;
+            while (matcher.find()) {
+                iter.add(string.substring(lastIndex, matcher.start()));
+                iter.add(matcher.group(1));
+                lastIndex = matcher.end();
+            }
+            iter.add(string.substring(lastIndex, string.length()));
+        }
+    }
+
+    private void filterColors() {
+        ListIterator<Object> iter = tokens.listIterator();
+        while (iter.hasNext()) {
+            Object o = iter.next();
+            if (!(o instanceof String)) continue;
+            iter.remove();
+            String string = ChatColor.translateAlternateColorCodes('&', (String)o);
+            Pattern pattern = Pattern.compile("<([^>]+)>");
+            Matcher matcher = pattern.matcher(string);
+            int lastIndex = 0;
+            while (matcher.find()) {
+                Color color = Color.getByName(matcher.group(1));
+                if (color == null) continue;
+                iter.add(string.substring(lastIndex, matcher.start()));
+                lastIndex = matcher.end();
+                iter.add(color);
+            }
+            iter.add(string.substring(lastIndex, string.length()));
+        }
+    }
+
+    private void filterVariables() {
+        ListIterator<Object> iter = tokens.listIterator();
+        while (iter.hasNext()) {
+            Object o = iter.next();
+            if (!(o instanceof String)) continue;
+            iter.remove();
+            String string = (String)o;
+            Pattern pattern = Pattern.compile("\\{([^\\}]+)\\}");
+            Matcher matcher = pattern.matcher(string);
+            int lastIndex = 0;
+            while (matcher.find()) {
+                iter.add(string.substring(lastIndex, matcher.start()));
+                lastIndex = matcher.end();
+                Variable var = new Variable();
+                var.key = matcher.group(1);
+                environment.put(var.key, "{" + matcher.group(1) + "}");
+                iter.add(var);
+            }
+            iter.add(string.substring(lastIndex, string.length()));
+        }
+    }
+
+    public List<String> compile() {
+        List<String> result = new ArrayList<String>();
+        StringBuilder sb = new StringBuilder();
+        for (Object o : tokens) {
+            if (o == endl) {
+                result.add(sb.toString());
+                sb = new StringBuilder();
+            } else if (o instanceof Variable) {
+                sb.append(environment.get(((Variable)o).key).toString());
+            } else {
+                sb.append(o.toString());
+            }
+        }
+        return result;
+    }
+
+    public List<String> compileNoColor() {
+        List<String> result = new ArrayList<String>();
+        StringBuilder sb = new StringBuilder();
+        for (Object o : tokens) {
+            if (o == endl) {
+                result.add(sb.toString());
+                sb = new StringBuilder();
+            } else if (o instanceof Variable) {
+                sb.append(environment.get(((Variable)o).key).toString());
+            } else if (o instanceof Color) {
+                continue;
+            } else {
+                sb.append(o.toString());
+            }
+        }
+        return result;
+    }
+
+    public Message set(String key, Object value) {
+        environment.put(key, value);
+        return this;
+    }
+
+    public Message set(Item item) {
+        set("item", item.getName());
+        set("itemdesc", item.getDescription());
+        set("amount", item.getAmount());
+        set("totalamount", item.getAmount().getInt());
+        set("enchantments", item.getItemInfo());
+        set("iteminfo", item.getItemInfo());
+        return this;
+    }
+
+    public Message set(Auction auction) {
+        set("id", auction.getId());
+        set(auction.getItem());
+        set("owner", auction.getOwner().getName());
+        if (auction.getWinner() != null) set("winner", auction.getWinner().getName());
+        set("minbid", auction.getMinimalBid());
+        set("maxbid", auction.getMaxBid());
+        set("price", auction.getWinningBid());
+        set("fee", auction.getFee());
+        set("state", auction.getState());
+        set("timeleft", new AuctionTime(auction.getTimeLeft()));
+        return this;
+    }
+
+    public Message set(CommandSender sender) {
+        set("player", sender.getName());
+        return this;
+    }
+
+    public Message set(Merchant merchant) {
+        set("player", merchant.getName());
+        return this;
+    }
+
+    public Message set(Auction auction, CommandSender sender) {
+        set(auction);
+        set(sender);
+        return this;
+    }
+
+    public Message set(Auction auction, Merchant merchant) {
+        set(auction);
+        set(merchant);
+        return this;
+    }
+
+    public Message set(AuctionBidEvent event) {
+        set(event.getAuction());
+        set(event.getBidder());
+        set("bid", event.getAmount());
+        if (event.getOldWinner() != null) set("oldwinner", event.getOldWinner().getName());
+        set("oldprice", event.getOldPrice());
+        return this;
+    }
+
+    public Message set(AuctionTimeChangeEvent event) {
+        set(event.getAuction());
+        set(event.getSender());
+        set("newtimeleft", new AuctionTime(event.getDelay()));
+        return this;
+    }
+
+    @Override
+    protected Message clone() {
+        return new Message(this);
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        for (String line : compileNoColor()) sb.append(line).append('\n');
+        if (sb.length() > 0) sb.setLength(sb.length() - 1);
+        return sb.toString();
+    }
 }
